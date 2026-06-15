77483fb8d5427fdef1ca31afe0773c454cb3cea6
Yaw D Acquah <acquahyaw13@gmail.com>
2026-06-15 10:27:01 -0700

    Updated faster_nim_start with a main wrapper

--- Nim/faster_nim_start.py
+++ Nim/faster_nim_start.py
@@ -3,8 +3,31 @@ import display
 from numba import njit
 # import time
 
-# Using loops + njit instead of using np.arange/np.where for faster code
+def main():
+    grid_size = int(input("Size of the grid you want to see:\n"))
+    is_winner = (input("Winner or loser? (W/L)\n") == 'W')
+    desired_level = int(input("x-level?\n"))
 
+    # Initialize Wx as all 0s. We don't need Lx because in Nim, W0 = MW0
+    Wx = np.zeros((grid_size,grid_size),dtype=np.bool_)
+
+    # generate_Wx(Wx, desired_level)   # Warm up benchmark
+    # start = time.perf_counter()     # Benchmark time start
+
+    Wx = generate_Wx(Wx, desired_level)
+
+    # end = time.perf_counter()       # Benchmark time stop
+    # print(f"Execution time: {end - start:.6f} seconds")
+
+    if(is_winner == True):
+        # print(Wx.astype(int))
+        display.output(Wx, True, desired_level)
+    else:
+        Lx = supermex(Wx)
+        # print(Lx.astype(int))
+        display.output(Lx, False, desired_level)
+
+# Using loops + njit instead of using np.arange/np.where for faster code
 @njit
 def generate_Wx(Wx, desired_level):
     if(desired_level == 0): 
@@ -37,26 +60,6 @@ def supermex(Wx):
     return MWx
 
 #---------------------------- Running Main Function ----------------------------
+if __name__ == "__main__":
+    main()
 
-grid_size = int(input("Size of the grid you want to see:\n"))
-is_winner = (input("Winner or loser? (W/L)\n") == 'W')
-desired_level = int(input("x-level?\n"))
-
-# Initialize Wx as all 0s. We don't need Lx because in Nim, W0 = MW0
-Wx = np.zeros((grid_size,grid_size),dtype=np.bool_)
-
-# generate_Wx(Wx, desired_level)   # Warm up benchmark
-# start = time.perf_counter()     # Benchmark time start
-
-Wx = generate_Wx(Wx, desired_level)
-
-# end = time.perf_counter()       # Benchmark time stop
-# print(f"Execution time: {end - start:.6f} seconds")
-
-if(is_winner == True):
-    # print(Wx.astype(int))
-    display.output(Wx, True, desired_level)
-else:
-    Lx = supermex(Wx)
-    # print(Lx.astype(int))
-    display.output(Lx, False, desired_level)
